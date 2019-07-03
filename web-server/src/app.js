98f1e36f1fe4dace8c47a332a16f1a5fd5101f6c
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2019-07-03 02:20:21 +0200

    set dynamic port

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
@@ -92,6 +93,6 @@ app.get('*', (req, res) => {
     })
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.');
+app.listen(port, () => {
+    console.log('Server is up on port ' + port);
 });
