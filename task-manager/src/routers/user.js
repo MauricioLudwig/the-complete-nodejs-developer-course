c0dec78342ee4f584d379c42607b0342b6f8f60e
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2019-07-21 07:31:25 +0200

    enable file upload

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const multer = require('multer');
+const sharp = require('sharp');
 const router = new express.Router();
 const auth = require('../middleware/auth');
 const User = require('../models/user');
+
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload a valid image.'));
+        }
+
+        cb(undefined, true);
+    }
+});
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
@@ -55,6 +70,15 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user);
 });
 
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+    req.user.avatar = buffer;
+    await req.user.save();
+    res.send();
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+});
+
 router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['name', 'email', 'password', 'age'];
@@ -79,6 +103,27 @@ router.delete('/users/me', auth, async (req, res) => {
         res.send(req.user);
     } catch (e) {
         res.status(500).send();
+    }
+});
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send();
+});
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!user || !user.avatar) {
+            throw new Error();
+        }
+
+        res.set('Content-Type', 'image/png');
+        res.send(user.avatar);
+    } catch (e) {
+        res.status(404).send();
     }
 });
 
