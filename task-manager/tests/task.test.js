6415f583cb82f75f3d543a57e1a8aea5f9e706b5
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2019-08-11 20:17:05 +0200

    testing with jest

---
+++ task-manager/tests/task.test.js
@@ -0,0 +1,46 @@
+const request = require('supertest');
+const app = require('../src/app');
+const Task = require('../src/models/task');
+const {
+    userOne,
+    userTwo,
+    taskOne,
+    setupDatabase
+} = require('./fixtures/db');
+
+beforeEach(setupDatabase);
+
+test('Should create task for user', async () => {
+    const response = await request(app)
+        .post('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            description: 'From my test'
+        })
+        .expect(201);
+
+    const task = await Task.findById(response.body._id);
+    expect(task).not.toBeNull();
+    expect(task.completed).toEqual(false);
+});
+
+test('Should return all tasks belonging to user one', async () => {
+    const response = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200);
+
+    expect(response.body.length).toBe(2);
+});
+
+test('Should not permit unauthorized deletion of task', async () => {
+    await request(app)
+        .delete(`/tasks/${taskOne._id}`)
+        .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+        .send()
+        .expect(404);
+
+    const task = await Task.findById(taskOne._id);
+    expect(task).not.toBeNull();
+});
